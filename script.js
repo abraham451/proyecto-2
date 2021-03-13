7df63ca68f03e29a37224117fe2acdf974470d3c
abraham <abrahamnuno7@gmail.com>
2021-03-13 14:40:19 -0600

    mi primer commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var autobiografia, este soy yo;
+
+autobiografia = "mi historia de vida"
+este soy yo = "mi cara"
+
+document.getElementById("autobiografia").innerHTML = autobiografia
+document.getElementById("este soy yo").innerHTML = este soy yo
